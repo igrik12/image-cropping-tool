99055e6813b5ae3d17adaed2a7bb2abf8a3e75cc
Igor <igrik12@hotmail.com>
2021-01-13 21:27:10 +0000

    working crop crop selection, but needs refactor

--- src/App.js
+++ src/App.js
@@ -1,10 +1,29 @@
-import Imagecropfeedback from './ImageCropFeedback';
+import React, { useState } from 'react';
+import ImageCropFeedback from './ImageCropFeedback';
 import './App.css';
 
 function App() {
+  const [area, setArea] = useState({ start: { x: 50, y: 50 }, end: { x: 350, y: 350 } });
+  const [startCropping, setStartCropping] = useState(false);
+
+  const handleOnAreaSelect = (coords) => {
+    if (startCropping) {
+      setArea({ start: area.start, end: coords });
+    } else {
+      setArea({ start: coords, end: coords });
+    }
+  };
+
   return (
     <div className='App'>
-      <Imagecropfeedback imageUrl='https://placekitten.com/600/600' />
+      <ImageCropFeedback
+        imageUrl='https://placekitten.com/600/600'
+        start={area.start}
+        end={area.end}
+        startCropping={startCropping}
+        setStartCropping={setStartCropping}
+        onAreaSelect={handleOnAreaSelect}
+      />
     </div>
   );
 }
--- src/ImageCropFeedback.js
+++ src/ImageCropFeedback.js
@@ -1,16 +1,17 @@
-import React, { useRef, useEffect, useLayoutEffect, forwardRef } from 'react';
+import React, { useRef, useEffect, useLayoutEffect, forwardRef, useState } from 'react';
 
-export default function Imagecropfeedback({
+export default function ImageCropFeedback({
   imageUrl,
-  top = { x0: 50, y0: 50, x1: 350, y1: 50 },
-  left = { x0: 50, y0: 50, x1: 50, y1: 350 },
-  right = { x0: 350, y0: 50, x1: 350, y1: 350 },
-  bottom = { x0: 50, y0: 350, x1: 350, y1: 350 },
+  start,
+  end,
   onAreaSelect = () => {},
+  startCropping,
+  setStartCropping,
 }) {
   const canvasRef = useRef();
   const imageRef = useRef();
   const contextRef = useRef();
+  const firstRun = useRef(true);
 
   useLayoutEffect(() => {
     const currentCanvas = canvasRef.current;
@@ -26,28 +27,51 @@ export default function Imagecropfeedback({
 
   useEffect(() => {
     const drawLineCtx = drawLine(contextRef.current);
+    let top;
+    let left;
+    let right;
+    let bottom;
+    if (firstRun.current) {
+      top = { x0: 50, y0: 50, x1: 350, y1: 50 };
+      left = { x0: 50, y0: 50, x1: 50, y1: 350 };
+      right = { x0: 350, y0: 50, x1: 350, y1: 350 };
+      bottom = { x0: 50, y0: 350, x1: 350, y1: 350 };
+      firstRun.current = false;
+    } else {
+      top = { x0: start.x, y0: start.y, x1: end.x, y1: start.y };
+      left = { x0: start.x, y0: start.y, x1: start.x, y1: end.y };
+      right = { x0: end.x, y0: start.y, x1: end.x, y1: end.y };
+      bottom = { x0: start.x, y0: end.y, x1: end.x, y1: end.y };
+    }
+    contextRef.current.clearRect(0, 0, canvasRef.current.width, canvasRef.current.height);
+    contextRef.current.drawImage(imageRef.current, 0, 0);
     drawLineCtx(top);
     drawLineCtx(left);
     drawLineCtx(right);
     drawLineCtx(bottom);
-  }, [top, left, right, bottom]);
+  }, [start, end]);
 
   return (
     <div>
-      <canvas onMouseMove={handleMouseMove(onAreaSelect)} onClick={handleClick(onAreaSelect)} ref={canvasRef}></canvas>
+      <canvas
+        onClick={handleClick(onAreaSelect, setStartCropping)}
+        onMouseMove={handleMouseMove(onAreaSelect, startCropping)}
+        ref={canvasRef}
+      ></canvas>
       <Image ref={imageRef} url={imageUrl} style={{ opacity: 0 }} />
     </div>
   );
 }
 
-const handleClick = (onAreaSelect) => (event) => {
-  console.log(event.clientX);
-  console.log(event.clientY);
+const handleClick = (onAreaSelect, setStartCropping, ctx) => (event) => {
+  onAreaSelect({ x: event.clientX, y: event.clientY });
+  setStartCropping((state) => !state);
 };
 
-const handleMouseMove = (onAreaSelect) => (event) => {
-  console.log(event.clientX);
-  console.log(event.clientY);
+const handleMouseMove = (onAreaSelect, startCropping) => (event) => {
+  if (startCropping) {
+    onAreaSelect({ x: event.clientX, y: event.clientY });
+  }
 };
 
 const drawLine = (canvasContext) => (coords, style = {}) => {
